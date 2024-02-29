1f027f80b02588d9089c2c5ca216a66723457e78
lum-jashari <lumijashari@hotmail.com>
2024-02-29 01:44:51 +0100

    check/uncheck todo functionality implemented

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,15 +5,26 @@ const props = defineProps({
         type: Object,
         required: true,
     },
+    index: {
+        type: Number,
+        required: true,
+    },
 });
+defineEmits(["toggle-complete"]);
 </script>
 
 <template>
     <li>
-        <input type="checkbox" :checked="todo.isCompleted" />
+        <input
+            type="checkbox"
+            :checked="todo.isCompleted"
+            @input="$emit('toggle-complete', index)"
+        />
         <div class="todo">
             <input v-if="todo.isEditing" type="text" :value="todo.todo" />
-            <span v-else>{{ todo.todo }}</span>
+            <span v-else :class="{ 'completed-todo': todo.isCompleted }">{{
+                todo.todo
+            }}</span>
         </div>
         <div class="todo-actions">
             <Icon
@@ -75,6 +86,10 @@ li {
 
     .todo {
         flex: 1;
+
+        .completed-todo {
+            text-decoration: line-through;
+        }
 
         input[type="text"] {
             width: 100%;
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -17,6 +17,10 @@ const createTodo = (todo) => {
         isEditing: null,
     });
 };
+
+const toggleTodoComplete = (todoPos) => {
+    todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+};
 </script>
 
 <template>
@@ -24,7 +28,13 @@ const createTodo = (todo) => {
         <h1>Create Todo</h1>
         <TodoCreator @create-todo="createTodo" />
         <ul class="todo-list" v-if="todoList.length > 0">
-            <TodoItem v-for="todo in todoList" :key="todo.id" :todo="todo" />
+            <TodoItem
+                v-for="(todo, index) in todoList"
+                :key="todo.id"
+                :todo="todo"
+                :index="index"
+                @toggle-complete="toggleTodoComplete"
+            />
         </ul>
         <p v-else class="todos-msg">
             <Icon
